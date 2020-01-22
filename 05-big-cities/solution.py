cb9a4082e7c4ad54a83471f5815df1b7476f0a5d
Oleksii Tsvietnov <vorakl@protonmail.com>
2020-01-22 21:52:02 +0100

    Fix typo

--- 05-big-cities/solution.py
+++ 05-big-cities/solution.py
@@ -4,7 +4,6 @@ import requests
 import csv
 
 URL = 'https://gist.githubusercontent.com/reuven/77edbb0292901f35019f17edb9794358/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
-
 FIELDS = ('city', 'state', 'population', 'rank')
 
 try:
